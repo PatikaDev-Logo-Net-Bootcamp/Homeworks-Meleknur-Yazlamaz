8940460992921111518032cc61a8c0b0449dacdd
melekny <meleknuryazlamaz@gmail.com>
2022-03-05 02:38:40 +0300

    Add project files

--- Homework-1/Homework-1/Controllers/HomeController.cs
+++ Homework-1/Homework-1/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace Homework_1.Controllers
 {
     public class HomeController : Controller
@@ -18,6 +19,55 @@ namespace Homework_1.Controllers
             _logger = logger;
         }
 
+        // "Get" Method for Login Page
+        public IActionResult UserForm()
+        {
+            var user = new UserViewModel()
+            {
+
+            };
+            return View(user);
+
+        }
+
+        // "Post" Method for Login Page
+        [HttpPost]
+        public IActionResult UserForm(UserViewModel model)
+        {
+            var user = new Response
+            {
+                Data = "Null",
+                Success = false,
+                Error = "Hatalı Giriş!"
+
+            };
+
+            // Checking server-side validation proceess
+            if (ModelState.IsValid)
+            {
+                var newUser = new Response
+                {
+                    Data = "Giriş İşlemi Başarılı! Hoşgeldiniz :)",
+                    Success = true,
+                    Error = "Null"
+
+                };
+
+                // Transferring the data from Controller to View
+                ViewData["Data"] = "<div class='data-valid'>" + "Data: " + newUser.Data + "</div>";
+                ViewData["Success"] = "<div class='success-valid'>" + "Success: " + newUser.Success + "</div>";
+                ViewData["Error"] = "<div class='error-valid'>" + "Error: " + newUser.Error + "</div>";
+
+                return View();
+            }
+
+            ViewData["Data"] = "<div class='data-unvalid'>" + "Data: " + user.Data + "</div>";
+            ViewData["Success"] = "<div class='success-unvalid'>" + "Success: " + user.Success + "</div>";
+            ViewData["Error"] = "<div class='error-unvalid'>" + "Error: " + user.Error + "</div>";
+
+            return View(model);
+        }
+                                                                                   
         public IActionResult Index()
         {
             return View();
@@ -35,3 +85,4 @@ namespace Homework_1.Controllers
         }
     }
 }
+
---
+++ Homework-1/Homework-1/Models/ResponseViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Homework_1.Models
+{
+    public class Response
+    {
+        public bool Success { get; set; }
+
+        public string Error { get; set; }
+
+        public string Data { get; set; }
+
+    }
+}

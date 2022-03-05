eb959801df4ad9c9c1181097b8e8155c99d3eb16
melekny <meleknuryazlamaz@gmail.com>
2022-03-05 03:23:08 +0300

    Update project files

--- Homework-1/Homework-1/Models/UserViewModel.cs
+++ Homework-1/Homework-1/Models/UserViewModel.cs
@@ -21,15 +21,13 @@ namespace Homework_1.Models
         [DataType(DataType.EmailAddress)]
         [Required(ErrorMessage = "E-mail alanı zorunludur!")]
         [RegularExpression("^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$", ErrorMessage = "Geçersiz giriş! Lütfen geçerli bir E-mail adresi giriniz.")]
-
         public string Email { get; set; }
 
         [Display(Name = "Şifre")]
         [DataType(DataType.Password)]
         [Required(ErrorMessage = "Şifre alanı zorunludur!")]
-        [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", ErrorMessage = "Geçersiz Şifre! Şifreniz; en az bir küçük harf, bir büyük harf ve bir sayı dahil olmak üzere 8 karakterden oluşmalıdır.")]
-
-        public string Password { get; set; }
+        [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", ErrorMessage = "Geçersiz Şifre! Şifreniz; en az bir büyük harf, bir karakter ve bir sayı dahil olmak üzere 8 karakterden oluşmalıdır.")]
+         public string Password { get; set; }
     }
 }
 
